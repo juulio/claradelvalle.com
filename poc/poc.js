d10c0803c851873115d8b02eaa7c9a643b5f2ac7
Julio Del Valle <jdelvallegam@microsoft.com>
2023-11-21 22:49:41 -0600

    Updating dist folder

--- poc/poc.js
+++ poc/poc.js
@@ -25,18 +25,22 @@ function animate() {
   if(horizontalDirection == 'derecha' && verticalDirection == 'adelante') {
     cube.position.x += 0.002;
     cube.position.y += 0.002;
+    console.log('entro al 1');
   }
   if(horizontalDirection == 'izquierda' && verticalDirection == 'adelante' ) {
     cube.position.x -= 0.002;
     cube.position.y += 0.002;
+    console.log('entro al 2');
   }
   if(horizontalDirection == 'derecha' && verticalDirection == 'atras' ) {
-  cube.position.x += 0.002;
-  cube.position.y -= 0.002;
+    cube.position.x += 0.002;
+    cube.position.y -= 0.002;
+    console.log('entro al 3');
   }
   if(horizontalDirection == 'izquierda' && verticalDirection == 'atras' )   {
-  cube.position.x -= 0.002;
-  cube.position.y -= 0.002;
+    cube.position.x -= 0.002;
+    cube.position.y -= 0.002;
+    console.log('entro al 4');
   }
 	renderer.render( scene, camera );
 }
