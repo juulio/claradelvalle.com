af352cd9621e169d4f42f4ddc045e218eefc0b3a
Julio Del Valle <jdelvallegam@microsoft.com>
2023-11-21 22:00:59 -0600

    updating L poc

--- poc/poc.js
+++ poc/poc.js
@@ -44,30 +44,30 @@ demo_button.addEventListener('click',()=>{
 // Sensors functionality
 let is_running = false;
 
-demo_button.onclick = function(e) {
-  e.preventDefault();
+// demo_button.onclick = function(e) {
+//   e.preventDefault();
   
-  // Request permission for iOS 13+ devices
-  if (
-    DeviceMotionEvent &&
-    typeof DeviceMotionEvent.requestPermission === "function"
-  ) {
-    DeviceMotionEvent.requestPermission();
-  }
+//   // Request permission for iOS 13+ devices
+//   if (
+//     DeviceMotionEvent &&
+//     typeof DeviceMotionEvent.requestPermission === "function"
+//   ) {
+//     DeviceMotionEvent.requestPermission();
+//   }
   
-  if (is_running){
-    window.removeEventListener("deviceorientation", handleOrientation);
-    demo_button.innerHTML = "Start demo";
-    demo_button.classList.add('btn-success');
-    demo_button.classList.remove('btn-danger');
-    is_running = false;
-  }else{
-    window.addEventListener("deviceorientation", handleOrientation);
-    demo_button.classList.remove('btn-success');
-    demo_button.classList.add('btn-danger');
-    is_running = true;
-  }
-};
+//   if (is_running){
+//     window.removeEventListener("deviceorientation", handleOrientation);
+//     demo_button.innerHTML = "Start demo";
+//     demo_button.classList.add('btn-success');
+//     demo_button.classList.remove('btn-danger');
+//     is_running = false;
+//   }else{
+//     window.addEventListener("deviceorientation", handleOrientation);
+//     demo_button.classList.remove('btn-success');
+//     demo_button.classList.add('btn-danger');
+//     is_running = true;
+//   }
+// };
 
 
 function handleOrientation(event) {
