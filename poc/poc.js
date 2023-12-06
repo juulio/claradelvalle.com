e774a7eba90753ba95713589ec4782a3ff772055
Julio Del Valle <jdelvallegam@microsoft.com>
2023-12-05 22:30:54 -0600

    Adding responsive functionality to screen edges

--- poc/poc.js
+++ poc/poc.js
@@ -3,7 +3,9 @@ import * as THREE from 'three';
 
 //----------------------------------------------------------------
 // General functionality 
-let demo_button = document.getElementById("start_demo");
+const demo_button = document.getElementById("start_demo");
+const splashScreen = document.querySelector('.splash');
+
 let horizontalAcceleration = 0;
 let verticalAcceleration = 0;
 //----------------------------------------------------------------
@@ -14,8 +16,43 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 0.4, 0.4, 0.4 );
+// const vector = new THREE.Vector3(0, 3.8, 0);
+// vector.project(camera);
+// vector.x = Math.round((0.5 + vector.x / 2) * (renderer.domElement.width / window.devicePixelRatio));
+// vector.y = Math.round((0.5 - vector.y / 2) * (renderer.domElement.height / window.devicePixelRatio));
+
+// console.log("Canvas width: " + renderer.domElement.width);
+// console.log("Canvas height: " +  renderer.domElement.height);
+// console.log("V: " + vector.x + " " + vector.y);
+
+
+// This material is used in all cubes, for the POC
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+//----------------------------------------------------------------
+
+const translate2dTo3d = (posX, posY) => {
+  var vec = new THREE.Vector3();
+  var pos = new THREE.Vector3();
+
+  // calculate position in normalized device coordinates (-1 to +1) for both components
+  vec.set(
+      ( posX / window.innerWidth ) * 2 - 1,
+      - ( posY / window.innerHeight ) * 2 + 1,
+      0.5 );
+
+  vec.unproject( camera );
+
+  vec.sub( camera.position ).normalize();
+
+  var distance = - camera.position.z / vec.z;
+
+  pos.copy( camera.position ).add( vec.multiplyScalar( distance ) );
+  console.log("translated: (" + posX + ", " + posY + ") to (" + pos.x + ", " + pos.y + ")");
+  return pos;
+}
+
+//----------------------------------------------------------------
+const geometry = new THREE.BoxGeometry( 0.4, 0.4, 0.4 );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
@@ -24,23 +61,18 @@ camera.position.z = 5;
 function animate() {
 	requestAnimationFrame( animate );
 
+  if(cube.position.x >= 1.7 || cube.position.x <= -1.7){
+    horizontalAcceleration *= -1;
+  }
+  if(cube.position.y >= 3.7 || cube.position.y <= -3.7){ 
+    verticalAcceleration *= -1;
+  }
+
   cube.position.x += 0.001*horizontalAcceleration;
   cube.position.y -= 0.001*verticalAcceleration;
  
 	renderer.render( scene, camera );
 }
-animate();
-
-
-//----------------------------------------------------------------
-// splashScreen functionality
-var splashScreen = document.querySelector('.splash');
-splashScreen.addEventListener('click',()=>{
-    splashScreen.style.opacity = 0;
-    setTimeout(()=>{
-        splashScreen.classList.add('hidden')
-    },610)
-})
 
 //----------------------------------------------------------------
 // Sensors functionality
@@ -56,38 +88,69 @@ demo_button.onclick = function(e) {
   ) {
     DeviceMotionEvent.requestPermission();
   }
-  
-  if (is_running){
-    window.removeEventListener("deviceorientation", handleOrientation);
-    demo_button.innerHTML = "Start demo";
-    demo_button.classList.add('btn-success');
-    demo_button.classList.remove('btn-danger');
-    is_running = false;
-  }else{
+
     window.addEventListener("deviceorientation", handleOrientation);
     demo_button.classList.remove('btn-success');
     demo_button.classList.add('btn-danger');
     is_running = true;
-  }
 };
-
 
 function handleOrientation(event) {
   horizontalAcceleration = event.gamma;
   verticalAcceleration = event.beta;
-
   // console.log('x Acc: ', horizontalAcceleration + '  y Acc: ', verticalAcceleration);
-  // right and left movement of the device
-  if (event.gamma > 0) {
-      document.getElementById("rightLeft").innerHTML = "&rarr;";
-  } else {
-      document.getElementById("rightLeft").innerHTML = "&larr;";
-  }
-
-  // forward and backward movement of the device
-  if (event.beta > 0) {
-      document.getElementById("forwardBack").innerHTML = "&darr;";
-  } else {
-      document.getElementById("forwardBack").innerHTML = "&uarr;";
-  }
 }
+
+//----------------------------------------------------------------
+// Resize functionality
+const onWindowResize = (SCREEN_WIDTH, SCREEN_HEIGHT) => {
+  renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT); 
+  camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+}
+
+const setScreenEdges = (SCREEN_WIDTH, SCREEN_HEIGHT) => {
+  const topLeft = translate2dTo3d(0, 0);
+  const bottomRight = translate2dTo3d(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  // 4 GAME AREA EDGES
+  const topBottomEdgeGeometry = new THREE.BoxGeometry( bottomRight.x*2, 0.1, 0.1 );
+  const rightLeftEdgeGeometry = new THREE.BoxGeometry( 0.2, topLeft.y*2, 0.1 );
+
+  const topEdgeCube = new THREE.Mesh( topBottomEdgeGeometry, material );
+  const bottomEdgeCube = new THREE.Mesh( topBottomEdgeGeometry, material );
+  const leftEdgeCube = new THREE.Mesh( rightLeftEdgeGeometry, material );
+  const rightEdgeCube = new THREE.Mesh( rightLeftEdgeGeometry, material );
+
+  topEdgeCube.position.set(0, topLeft.y, 0)
+  bottomEdgeCube.position.set(0, -topLeft.y, 0);
+  leftEdgeCube.position.set(-topLeft.x, 0, 0);
+  rightEdgeCube.position.set(topLeft.x, 0, 0);
+
+  scene.add( topEdgeCube );
+  scene.add( bottomEdgeCube );
+  scene.add( leftEdgeCube );
+  scene.add( rightEdgeCube );
+
+}
+
+const init = () => {
+  const SCREEN_WIDTH = window.innerWidth;
+  const SCREEN_HEIGHT = window.innerHeight;
+
+  window.addEventListener( 'resize', onWindowResize, false );
+  
+  // splashScreen functionality
+  splashScreen.addEventListener('click',()=>{
+    splashScreen.style.opacity = 0;
+    setTimeout(()=>{splashScreen.classList.add('hidden')},610);
+    setScreenEdges(SCREEN_WIDTH, SCREEN_HEIGHT);
+  });
+  
+  onWindowResize(SCREEN_WIDTH, SCREEN_HEIGHT);
+  animate();
+}
+
+init();
